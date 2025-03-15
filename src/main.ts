7969e552e0158e45df4de06c6ec77a49f1af545f
felltrin <luciholzinger@gmail.com>
2025-03-14 22:18:23 -0700

    The scene has been drawn
    Signed-off-by: felltrin <luciholzinger@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -14,32 +14,56 @@
 
 import r from "raylib";
 
-const boxColor: r.Color = r.BROWN;
-const boxPosition: r.Vector2 = { x: 50, y: 50 };
-
 const screenWidth = 1280;
 const screenHeight = 720;
 
-r.InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+// scene constants
+const houseY: number = 550;
+const playerOneHouseSpec: r.Vector3 = { x: 200, y: houseY, z: 150 };
+const playerTwoHouseSpec: r.Vector3 = { x: 900, y: houseY, z: 150 };
+const ground: r.Vector3 = { x: 0, y: 700, z: 1300 };
+const houseColor: r.Color = { r: 0, g: 0, b: 128, a: 255 };
+
+const constructHouse = (playerSpec: r.Vector3, color: r.Color) => {
+  r.DrawRectangle(
+    playerSpec.x,
+    playerSpec.y,
+    playerSpec.z,
+    playerSpec.z,
+    color
+  );
+  r.DrawTriangle(
+    { x: playerSpec.x + 75, y: houseY - 125 },
+    { x: playerSpec.x - 50, y: houseY },
+    { x: playerSpec.x + playerSpec.z + 50, y: houseY },
+    color
+  );
+};
+
+r.InitWindow(screenWidth, screenHeight, "home feud");
 r.SetTargetFPS(60);
 
 while (!r.WindowShouldClose()) {
-  if (r.IsKeyDown(r.KEY_RIGHT)) {
-    boxPosition.x += 10;
-  }
-  if (r.IsKeyDown(r.KEY_LEFT)) {
-    boxPosition.x -= 10;
-  }
-  if (r.IsKeyDown(r.KEY_DOWN)) {
-    boxPosition.y += 10;
-  }
-  if (r.IsKeyDown(r.KEY_UP)) {
-    boxPosition.y -= 10;
-  }
-
+  // if (r.IsKeyDown(r.KEY_RIGHT)) {
+  //   boxPosition.x += 10;
+  // }
+  // if (r.IsKeyDown(r.KEY_LEFT)) {
+  //   boxPosition.x -= 10;
+  // }
+  // if (r.IsKeyDown(r.KEY_DOWN)) {
+  //   boxPosition.y += 10;
+  // }
+  // if (r.IsKeyDown(r.KEY_UP)) {
+  //   boxPosition.y -= 10;
+  // }
   r.BeginDrawing();
   r.ClearBackground(r.SKYBLUE);
-  r.DrawRectangle(boxPosition.x, boxPosition.y, 200, 200, boxColor);
+
+  // scene drawing
+  constructHouse(playerOneHouseSpec, houseColor);
+  constructHouse(playerTwoHouseSpec, houseColor);
+  r.DrawRectangle(ground.x, ground.y, ground.z, ground.z, r.BROWN);
+
   r.EndDrawing();
 }
 r.CloseWindow();
